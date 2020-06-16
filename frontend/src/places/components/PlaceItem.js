fa95a8a8be7f9affb92b0243dd33cbfd908b3748
Robert Laws <roblaws@me.com>
2020-06-16 11:03:37 +0300

    begin user list section

---
+++ frontend/src/places/components/PlaceItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlaceItem = () => {
+  return <div></div>;
+};
+
+export default PlaceItem;
---
+++ frontend/src/places/components/PlaceList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlaceList = () => {
+  return <div></div>;
+};
+
+export default PlaceList;
